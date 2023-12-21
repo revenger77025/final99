2410eee563a1222981afdb4b8468c6d621d5e698
balaji <skilledsoul1000@gmail.com>
2023-12-21 22:09:21 +0530

    second commit

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+
+
+import './styles/components/footer.css'
+import './styles/components/header.css'
+import './styles/components/main.css'
+import './styles/components/mobile-nav.css'
+import './styles/components/payments.css'
+import './styles/components/work.css'
+import './styles/style.css'
+import './styles/utils.css'
+
